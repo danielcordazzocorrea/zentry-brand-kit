328fc33f0ab77c04f2f596b0b4c7db1e634cbd6b
Daniel Cordazzo Correa <danielcordazzocorrea@gmail.com>
2025-11-10 10:41:37 -0300

    vite ajeitado

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/zentry-brand-kit/", // ← ADICIONE ESTA LINHA
   server: {
     host: "::",
     port: 8080,
